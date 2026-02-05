cad7e72478c9c9e714f09e605ffefb3836f1eaec
Dilara Akdeniz <dilaraakdeniz@Dilara-MacBook-Pro.local>
2026-02-05 15:16:57 +0300

    9.3 completed

--- HWS100/Day09.playground/Contents.swift
+++ HWS100/Day09.playground/Contents.swift
@@ -136,3 +136,55 @@ animate(duration: 3) {
     print("Fade out the image")
 }
 
+//MARK: How to accept functions as parameters
+
+func generator() -> Int {
+    Int.random(in: 1...10)
+}
+
+func makeArray(size: Int, using genarator: () -> Int) -> [Int] {
+    var numbers = [Int]()
+    
+    for _ in 0..<size {
+        let newNumber = generator()
+        numbers.append(newNumber)
+    }
+    
+    return numbers
+}
+
+
+
+let rolls = makeArray(size: 5) {
+    Int.random(in: 1...20)
+}
+
+print(rolls)
+
+func generateNumber() -> Int {
+    Int.random(in: 1...20)
+}
+
+let newRolls = makeArray(size: 50, using: generateNumber)
+print(newRolls)
+
+//Eğer birden fazla fonksiyonu parametre alan bir fonksiyon olursa:
+
+func doImportantWork(first: () -> Void, second: () -> Void, third: () -> Void) {
+    print("About to start first work")
+    first()
+    print("About to start second work")
+    second()
+    print("About to start third work")
+    third()
+    print("Done!")
+}
+
+//Bunu çağırırken de
+doImportantWork {
+    print("This is the first work")
+} second: {
+    print("This is the second work")
+} third: {
+    print("This is the third work")
+}
